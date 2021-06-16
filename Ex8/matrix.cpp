cdac1931c0957089f0cac7ec698ec9c9ecaf92fc
Geetha495 <cs20btech11011@iith.ac.in>
2021-06-16 13:56:00 +0530

    uploaded and updated

---
+++ Ex8/matrix.cpp
@@ -0,0 +1,169 @@
+#include<iostream>
+
+#define maxSize 5
+template<typename T>
+class Row;
+
+template<typename T>
+class Matrix
+{	
+	unsigned  m;
+	T mtrx[maxSize][maxSize];
+	Row<T> r;
+
+	public :
+	Matrix(unsigned _m,unsigned _n)
+	{
+		m=_m;
+		r.n=_n;
+	}
+	Row<T>& operator[](unsigned i)
+	{
+		if (i >= m) 
+		{
+			std::cout << "Matrix index out of bound, exiting";
+			exit(0);
+		}
+		r.row = mtrx[i];
+		return r;
+	}
+	Matrix operator+(Matrix B)
+	{
+		if(B.m==m &&   ( (B.r).n == (r.n)) )
+		{
+			Matrix C(m,r.n);
+			for(int i=0;i<m;i++)
+			{
+				for(int j=0;j<r.n;j++)
+				{
+					C[i][j]=mtrx[i][j]+B[i][j];
+				}
+			}
+			return C;
+		}
+		else{
+			std::cout << "Matrices can't be added, exiting";
+			exit(0);
+		}
+	}
+	Matrix operator-(Matrix B)
+	{
+		if(B.m==m &&   ( (B.r).n == (r.n)) )
+		{
+			Matrix C(m,r.n);
+			for(int i=0;i<m;i++)
+			{
+				for(int j=0;j<r.n;j++)
+				{
+					C[i][j]=mtrx[i][j]-B[i][j];
+				}
+			}
+			return C;
+		}
+		else{
+			std::cout << "Matrices can't be subtracted, exiting";
+			exit(0);
+		}
+	}
+
+	Matrix operator*(Matrix B)
+	{
+		if(r.n == B.m )
+		{
+			Matrix C(m,r.n);
+			for(int i=0;i<m;i++)
+			{
+				for(int j=0;j<(B.r).n;j++)
+				{
+					C[i][j]=0;  
+					for(int k=0;k<(B.r).n;k++)    
+					{    
+						C[i][j]+=mtrx[i][k]*B[k][j];    
+					}    
+				}
+			}
+			
+			return C;
+		}
+		else{
+			std::cout << "Matrices can't be multiplied, exiting";
+			exit(0);
+		}
+	}
+
+void display()
+{
+	for (int i=0; i<m; i++)
+	{
+		std::cout<<"\t";
+		for (int j=0; j<r.n; j++)
+			std::cout << mtrx[i][j] << " ";
+		std::cout << std::endl;
+	}
+}
+
+
+
+	Matrix operator^(int p)
+	{
+		if(p==-1)
+		{
+			if(m==r.n)
+			{
+				Matrix inv(m,m);
+				//need to write
+				
+			}
+		}
+
+
+	}
+};
+
+
+template<typename T>
+class Row  
+{
+	public :
+	T* row ={0};
+	unsigned n;
+	
+	T& operator[](unsigned j)
+	{
+		if (j >= n) 
+		{
+			std::cout << "Matrix index out of bound, exiting";
+			exit(0);
+		}
+		return row[j];
+	}
+};
+
+int main()
+{
+	Matrix<int> A(3,3),B(3,3);
+	for(int i=0;i<3;i++)
+	{
+		for(int j=0;j<3;j++)
+		{
+			A[i][j]= 1;
+			B[i][j]=1;
+		}		
+	}
+
+	std::cout<<"A: "<<std::endl;
+	A.display();
+
+	std::cout<<"B : "<<std::endl;
+	B.display();
+
+	std::cout<<"add : "<<std::endl;
+	(A+B).display();
+
+	std::cout<<"subtract : "<<std::endl;
+	(A-B).display();
+
+	std::cout<<"multiply : "<<std::endl;
+	(A*B).display();
+
+}
--- README.md
+++ README.md
@@ -14,7 +14,7 @@
 
 [7](https://github.com/Geetha495/SDF_Exercises/tree/main/Ex7). Explore which operators can not be overloaded.
 
-8. Implement a template class for matrix. Implement operations for addition, inverse, multiplication of matrices etc.
+[8](https://github.com/Geetha495/SDF_Exercises/tree/main/Ex8). Implement a template class for matrix. Implement operations for addition, inverse, multiplication of matrices etc.
 
 9. Implement an intermediate representation (IR) for an assignment statement. e.g., x=y+z*3+w/q; any such string should be parsed in a composite object asgn_statement_t. This can further have expr_t with subclasses for add_exprt, mul_exprt etc (expression for every kind of operation, both unary and binary , cover 5 operators _, -, /, * and %. Note that at the end (leaves) you will have either a const_t or a variable_t. A const_t will have a value. Implement an interpreter for such a statement which will evaluate the statement in a given context_t where context_t is nothing but a symbol table that maps variables to values. For example, the statement above under the context (y=5,z=10,w=3,q=2) is evaluated to x=36. Use visitor pattern to evaluate the expression. Also use visitor pattern to count the number of constants and variable in a statement.
 
