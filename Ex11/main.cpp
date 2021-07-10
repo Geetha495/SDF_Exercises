496091a93fb3daddd602fc2e49535a1f32e81575
Geetha495 <cs20btech11011@iith.ac.in>
2021-07-10 21:53:19 +0530

    used base class fn in derived class overidden fn

--- Ex11/main.cpp
+++ Ex11/main.cpp
@@ -5,7 +5,7 @@ template <typename T>
 class multiset_t
 {
 public:
-	virtual void insert_multiset(T t) = 0;
+	virtual void insert(T t) = 0;
 	virtual void remove(T t) = 0;
 	virtual void remove_all(T t) = 0;
 	virtual void print()=0;
@@ -16,12 +16,12 @@ class multisetAdapter : public multiset_t<T>, public std::map<T,T>
 {
 	static int index;
 public:
-	void insert_multiset(T t)
+	void insert(T t)
 	{
 		if(!(*this)[t])
 		{
 			++index;
-			this->insert(std::pair<T,T>(t,0));
+			this->std::map<T,T>::insert(std::pair<T,T>(t,0));
 			(*this)[t]=1;
 		}
 		else
@@ -69,8 +69,8 @@ int main()
 	multiset_t<int> *m = new multisetAdapter<int>;
 	for (int i = 1; i <= 5; i++)
 	{
-		m->insert_multiset(i);
-		m->insert_multiset(i+2);
+		m->insert(i);
+		m->insert(i+2);
 	}
 	std::cout<<"before :";
 	m->print();
